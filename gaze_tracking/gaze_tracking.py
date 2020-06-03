385350be630a8f66a2b44c9fa7c66b6c0de61b22
Ruby Werman <rubywerman@berkeley.edu>
2020-06-03 17:06:00 -0500

    reduce left/right to just screen

--- gaze_tracking/gaze_tracking.py
+++ gaze_tracking/gaze_tracking.py
@@ -99,12 +99,12 @@ class GazeTracking(object):
     def is_right(self):
         """Returns true if the user is looking to the right"""
         if self.pupils_located:
-            return self.horizontal_ratio() <= 0.35
+            return self.horizontal_ratio() <= 0.45
 
     def is_left(self):
         """Returns true if the user is looking to the left"""
         if self.pupils_located:
-            return self.horizontal_ratio() >= 0.65
+            return self.horizontal_ratio() >= 0.7
 
     def is_center(self):
         """Returns true if the user is looking to the center"""
